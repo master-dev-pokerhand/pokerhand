ea1455c867049c8ff409c2e827561331e5a1b66f
emanuel.passinato <emanuel.passinato@zgsolucoes.com.br>
2020-02-07 16:54:35 -0300

    Definindo vencedor e terminando metodo principal

--- src/main/groovy/services/RodadaService.groovy
+++ src/main/groovy/services/RodadaService.groovy
@@ -1,20 +1,46 @@
 package services
 
 import dominio.Categorias.CartaAlta
+import dominio.Categorias.Categoria
+import dominio.Categorias.DoisPares
+import dominio.Categorias.Flush
+import dominio.Categorias.FullHouse
+import dominio.Categorias.Quadra
+import dominio.Categorias.RoyalFlush
+import dominio.Categorias.Sequencia
+import dominio.Categorias.StraightFlush
+import dominio.Categorias.Trinca
+import dominio.Categorias.UmPar
 import dominio.Jogador
 import dominio.Mesa
 import dominio.Rodada
+import enuns.Categorias
 import groovy.json.internal.LazyMap
 
 class RodadaService {
 
-	Rodada criaRodada(String nomeRodada, LazyMap mesaEJogadores){
+	private List<Class> classesCategorias = [
+			RoyalFlush,
+			StraightFlush,
+			Quadra,
+			FullHouse,
+			Flush,
+			Sequencia,
+			Trinca,
+			DoisPares,
+			UmPar,
+			CartaAlta
+	]
+
+	Rodada criaRodada(String nomeRodada, LazyMap mesaEJogadores) {
 		MesaService mesaService = new MesaService()
 		JogadorService jogadorService = new JogadorService()
 		Rodada rodada = new Rodada()
 
 		Mesa mesa = mesaService.criaMesa(mesaEJogadores.get('board') as String)
-		List<Jogador> jogadores = jogadorService.criaJogadores(mesaEJogadores.findAll {it.key != 'board'} as LazyMap, mesa)
+		List<Jogador> jogadores = jogadorService.criaJogadores(mesaEJogadores.findAll {
+			it.key != 'board'
+		} as LazyMap, mesa)
 
 		rodada.mesa = mesa
 		rodada.jogadores = jogadores
@@ -22,11 +48,33 @@ class RodadaService {
 		return rodada
 	}
 
-	List<Rodada> criaRodadas(LazyMap rodadas){
+	List<Rodada> criaRodadas(LazyMap rodadas) {
 		List<Rodada> rodadasCriadas = []
 		rodadas.each { String nomeRodada, LazyMap mesaEJogadores ->
-			rodadasCriadas.add(criaRodada(nomeRodada,mesaEJogadores))
+			rodadasCriadas.add(criaRodada(nomeRodada, mesaEJogadores))
 		}
 		return rodadasCriadas
+	}
+
+	Jogador encontraVencedorRodada(Rodada rodada) {
+		Jogador vencedor = null
+
+		Map<Categorias, List<Jogador>> jogadoresPorCategoria = rodada.jogadores.groupBy { it.jogada.categoria }
+		List<Categorias> categoriasOrdenadas = jogadoresPorCategoria.keySet().sort(false) { Categorias categoria -> categoria.categoria }
+		Categorias maiorCategoria = categoriasOrdenadas.last()
+
+		List<Jogador> candidatos = jogadoresPorCategoria.get(maiorCategoria)
+
+		if (candidatos.size() == 1) {
+			vencedor = candidatos.first()
+		} else {
+			for (Class categoria in classesCategorias) {
+				Categoria categoriaClass =  categoria.newInstance() as Categoria
+				if(categoriaClass.categoria_enum == maiorCategoria){
+					vencedor = categoriaClass.desempate(candidatos)
+				}
+			}
+		}
+		return vencedor
 	}
 }
--- src/test/groovy/pokerhand.groovy
+++ src/test/groovy/pokerhand.groovy
@@ -1,15 +1,53 @@
+import dominio.Jogador
 import dominio.Rodada
+import services.RodadaService
 import spock.lang.Specification
 
 class pokerhand extends Specification{
 
 	void 'teste generico'(){
 		given:
-//		String json = ApiGetter.obtemJson()
-		List<Rodada> rodadas = CriadorObjetos.criaTodasAsRodadas('')
+		RodadaService rodadaService = new RodadaService()
+		String json = ApiGetter.obtemJson()
+		List<Rodada> rodadas = CriadorObjetos.criaTodasAsRodadas(json)
 		when:
-		true
+		StringBuffer stringBuffer = new StringBuffer()
+		rodadas.each {
+			Jogador vencedor = rodadaService.encontraVencedorRodada(it)
+			if(vencedor){
+				stringBuffer.append(vencedor.nome + ' Venceu a rodada\n')
+			} else {
+				stringBuffer.append('A rodada terminou em empate\n')
+			}
+		}
 		then:
-		true
+		stringBuffer
 	}
+
+	String jsonTeste = """[
+  {
+    "poker": {
+      "h0": {
+        "board": "2s Tc 9d 9c 7h",
+        "p0": "Ah Qc",
+        "p1": "4c Kd"
+      },
+      "h1": {
+        "board": "Ah 6h 2s 5h 7d",
+        "p0": "9h Jd",
+        "p1": "Qc 6s"
+      },
+      "h2": {
+        "board": "Jh 5h 9h 8h 8c",
+        "p0": "2c 3c",
+        "p1": "Qd 4d"
+      },
+      "h3": {
+        "board": "6c 6h 3c 4s 5s",
+        "p0": "8c Kc",
+        "p1": "3d Ah"
+      }
+    }
+  }
+]"""
 }
