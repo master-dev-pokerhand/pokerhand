3e06fab77d5787ea758a3524d7e3ae7816d77f36
Paulo Cesar <paulobrito@zgsolucoes.com.br>
2020-02-07 17:16:33 -0300

    Adicionado método de desempate para Full House

--- src/main/groovy/dominio/Categorias/FullHouse.groovy
+++ src/main/groovy/dominio/Categorias/FullHouse.groovy
@@ -4,21 +4,35 @@ import dominio.Carta
 import dominio.Jogador
 import enuns.Categorias
 import enuns.Nipe
+import enuns.Valor
 
 class FullHouse extends Categoria {
 
-    Categorias categoria_enum = Categorias.FULL_HOUSE
+	Categorias categoria_enum = Categorias.FULL_HOUSE
 
-    @Override
-    Boolean ehCategoria(List<Carta> cartas) {
-        Map<Nipe, List<Carta>> mapaCartas = obtemCartasComMesmoValor(cartas)
-        int quantidadeTrinca = mapaCartas.values().findAll { it -> it.size() == 3 }.size()
-        int quantidadePares = mapaCartas.values().findAll { it -> it.size() == 2}.size()
-        return quantidadeTrinca > 1 || quantidadeTrinca == 1 && quantidadePares > 0
-    }
+	@Override
+	Boolean ehCategoria(List<Carta> cartas) {
+		Map<Nipe, List<Carta>> mapaCartas = obtemCartasComMesmoValor(cartas)
+		int quantidadeTrinca = mapaCartas.values().findAll { it -> it.size() == 3 }.size()
+		int quantidadePares = mapaCartas.values().findAll { it -> it.size() == 2 }.size()
+		return quantidadeTrinca > 1 || quantidadeTrinca == 1 && quantidadePares > 0
+	}
 
-    @Override
-    Jogador desempate(List<Jogador> jogadores) {
-        return null
-    }
+	@Override
+	Jogador desempate(List<Jogador> jogadores) {
+		Jogador ganhador = jogadores.first()
+		Map<Valor, List<Carta>> mapaTrinca = obtemCartasComMesmoValor(ganhador.jogada.cartasDaJogada)
+		Integer somaTrinca = (Integer) mapaTrinca.values().findAll { List<Carta> cartaList -> cartaList.size() == 3 }.flatten().sum { Carta carta -> carta.valor.valor }
+		Integer somaAtual
+
+		for (int idx = 1; idx <  jogadores.size(); idx++) {
+			mapaTrinca = obtemCartasComMesmoValor(jogadores[idx].jogada.cartasDaJogada)
+			somaAtual = (Integer) mapaTrinca.values().findAll { List<Carta> cartaList -> cartaList.size() == 3 }.flatten().sum { Carta carta -> carta.valor.valor }
+			if (somaAtual > somaTrinca) {
+				ganhador = jogadores[idx]
+			}
+		}
+
+		return ganhador
+	}
 }
--- src/test/groovy/categorias/FullHouseSpec.groovy
+++ src/test/groovy/categorias/FullHouseSpec.groovy
@@ -2,36 +2,84 @@ package categorias
 
 import dominio.Carta
 import dominio.Categorias.Categoria
-import dominio.Categorias.Flush
 import dominio.Categorias.FullHouse
+import dominio.Jogada
+import dominio.Jogador
 import enuns.Nipe
 import enuns.Valor
 import spock.lang.Specification
 
 class FullHouseSpec extends Specification {
-    def 'verifica se a jogada é um fullhouse corretamente'() {
-        setup:
-        FullHouse fullHouse = new FullHouse()
+	def 'verifica se a jogada é um fullhouse corretamente'() {
+		setup:
+		FullHouse fullHouse = new FullHouse()
 
-        when:
-        boolean resultado = fullHouse.ehCategoria(cartas)
+		when:
+		boolean resultado = fullHouse.ehCategoria(cartas)
 
-        then:
-        resultado == resultadoEsperado
+		then:
+		resultado == resultadoEsperado
 
-        where:
-        cartas                                                                                  | resultadoEsperado
-        getCartasBase() + [new Carta(Valor.DAMA, Nipe.PAUS), new Carta(Valor.DOIS, Nipe.PAUS)]  | true
-        getCartasBase() + [new Carta(Valor.DAMA, Nipe.PAUS), new Carta(Valor.DAMA, Nipe.OUROS)] | true
-        getCartasBase() + [new Carta(Valor.DOIS, Nipe.PAUS), new Carta(Valor.AS, Nipe.OUROS)]   | false
-    }
+		where:
+		cartas                                                                                  | resultadoEsperado
+		getCartasBase() + [new Carta(Valor.DAMA, Nipe.PAUS), new Carta(Valor.DOIS, Nipe.PAUS)]  | true
+		getCartasBase() + [new Carta(Valor.DAMA, Nipe.PAUS), new Carta(Valor.DAMA, Nipe.OUROS)] | true
+		getCartasBase() + [new Carta(Valor.DOIS, Nipe.PAUS), new Carta(Valor.AS, Nipe.OUROS)]   | false
+	}
 
-    List<Carta> getCartasBase() {
-        List<Carta> cartas = new ArrayList<>()
-        cartas.add(new Carta(Valor.REI, Nipe.PAUS))
-        cartas.add(new Carta(Valor.REI, Nipe.COPAS))
-        cartas.add(new Carta(Valor.REI, Nipe.ESPADAS))
-        cartas.add(new Carta(Valor.DAMA, Nipe.COPAS))
-        return cartas
-    }
+	def 'desempata jogada entre dois Full Houses'() {
+		setup:
+		Jogador jogador1 = new Jogador()
+		Jogador jogador2 = new Jogador()
+
+		Jogada jogada1 = new Jogada()
+		Jogada jogada2 = new Jogada()
+
+		jogador1.jogada = jogada1
+		jogador2.jogada = jogada2
+
+		jogador1.jogada.cartasDaJogada = getCartas_p1()
+		jogador2.jogada.cartasDaJogada = getCartas_p2()
+
+		Categoria categoria = new FullHouse()
+
+		when:
+		Jogador jogador = categoria.desempate([jogador1, jogador2])
+
+		then:
+		jogador == jogador1
+	}
+
+	List<Carta> getCartas_p2() {
+		List<Carta> cartas = new ArrayList<>()
+		cartas.add(new Carta(Valor.TRES, Nipe.OUROS))
+		cartas.add(new Carta(Valor.TRES, Nipe.ESPADAS))
+		cartas.add(new Carta(Valor.TRES, Nipe.PAUS))
+		cartas.add(new Carta(Valor.DEZ, Nipe.OUROS))
+		cartas.add(new Carta(Valor.DOIS, Nipe.PAUS))
+		cartas.add(new Carta(Valor.DOIS, Nipe.PAUS))
+		cartas.add(new Carta(Valor.QUATRO, Nipe.PAUS))
+		return cartas
+	}
+
+	List<Carta> getCartas_p1() {
+		List<Carta> cartas = new ArrayList<>()
+		cartas.add(new Carta(Valor.REI, Nipe.OUROS))
+		cartas.add(new Carta(Valor.QUATRO, Nipe.ESPADAS))
+		cartas.add(new Carta(Valor.REI, Nipe.PAUS))
+		cartas.add(new Carta(Valor.SETE, Nipe.OUROS))
+		cartas.add(new Carta(Valor.REI, Nipe.PAUS))
+		cartas.add(new Carta(Valor.QUATRO, Nipe.PAUS))
+		cartas.add(new Carta(Valor.CINCO, Nipe.PAUS))
+		return cartas
+	}
+
+	List<Carta> getCartasBase() {
+		List<Carta> cartas = new ArrayList<>()
+		cartas.add(new Carta(Valor.REI, Nipe.PAUS))
+		cartas.add(new Carta(Valor.REI, Nipe.COPAS))
+		cartas.add(new Carta(Valor.REI, Nipe.ESPADAS))
+		cartas.add(new Carta(Valor.DAMA, Nipe.COPAS))
+		return cartas
+	}
 }
