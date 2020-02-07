3a00b57e3e5bfe12c9e3c9118cbe1b628de74436
luizcintra <luiz.cintra@zgsolucoes.com.br>
2020-02-07 16:46:32 -0300

    desempate StraightFlush

--- src/main/groovy/dominio/Categorias/StraightFlush.groovy
+++ src/main/groovy/dominio/Categorias/StraightFlush.groovy
@@ -16,13 +16,27 @@ class StraightFlush extends Sequencia {
 
     @Override
     Jogador desempate(List<Jogador> jogadores) {
-        return null
+        List<Jogador> campeao = [jogadores.first()]
+        for (int i = 1; i < jogadores.size(); i++) {
+            List<Carta> straightFlushCampeao = getStraightFlush(campeao.first().jogada.cartasDaJogada)
+            List<Carta> straightFlushJogador = getStraightFlush(jogadores[i].jogada.cartasDaJogada)
+            Carta maiorCartaCampeao = obtemMaiorCarta(straightFlushCampeao)
+            Carta maiorCartaJogador = obtemMaiorCarta(straightFlushJogador)
+
+            if(maiorCartaJogador && maiorCartaJogador.valor.valor > maiorCartaCampeao.valor.valor){
+                campeao.clear()
+                campeao.add(jogadores[i])
+            } else if (maiorCartaJogador && maiorCartaJogador == maiorCartaCampeao){
+                campeao.add(jogadores[i])
+            }
+        }
+        return campeao.size() == 1 ? campeao.first() : null
     }
 
     List<Carta> getStraightFlush(List<Carta> cartas) {
         Map<Valor, List<Carta>> mapaNipe = obtemCartasComMesmoNipe(cartas)
         List<Carta> cartasFlush = mapaNipe.values().findAll { it -> it.size() == 5 }
-        if(!cartasFlush.empty) {
+        if (!cartasFlush.empty) {
             List<Carta> cartasOrdenadas = cartasFlush.first().sort { it -> it.valor.valor }
             return getSequencia(cartasOrdenadas)
         } else {
--- src/test/groovy/categorias/StraightFlushSpec.groovy
+++ src/test/groovy/categorias/StraightFlushSpec.groovy
@@ -2,6 +2,8 @@ package categorias
 
 import dominio.Carta
 import dominio.Categorias.StraightFlush
+import dominio.Jogada
+import dominio.Jogador
 import enuns.Nipe
 import enuns.Valor
 import spock.lang.Specification
@@ -23,9 +25,35 @@ class StraightFlushSpec extends Specification {
         getCartasBase() + [new Carta(Valor.SETE, Nipe.COPAS)] | true
     }
 
+    def 'o desempate do straight flush deve executar corretamente'() {
+        setup:
+        StraightFlush straightFlush = new StraightFlush()
+
+        Jogada jogada1 = new Jogada()
+        Jogador jogador1 = new Jogador()
+        jogador1.jogada = jogada1
+        jogador1.jogada.cartasDaJogada = getCartasBase() + [new Carta(Valor.VALETE, Nipe.COPAS)]
+
+        Jogada jogada2 = new Jogada()
+        Jogador jogador2 = new Jogador()
+        jogador2.jogada = jogada2
+        jogador2.jogada.cartasDaJogada = getCartasBase() + [new Carta(Valor.SEIS, Nipe.COPAS)]
+
+        Jogada jogada3 = new Jogada()
+        Jogador jogador3 = new Jogador()
+        jogador3.jogada = jogada3
+        jogador3.jogada.cartasDaJogada = getCartasBase() + [new Carta(Valor.DOIS, Nipe.PAUS)]
+
+        when:
+        Jogador campeao = straightFlush.desempate([jogador1, jogador2, jogador3])
+
+        then:
+        campeao == jogador1
+    }
+
     List<Carta> getCartasBase() {
         List<Carta> cartas = new ArrayList<>()
-        cartas.add(new Carta(Valor.SEIS, Nipe.COPAS))
+        cartas.add(new Carta(Valor.SETE, Nipe.COPAS))
         cartas.add(new Carta(Valor.DEZ, Nipe.COPAS))
         cartas.add(new Carta(Valor.REI, Nipe.ESPADAS))
         cartas.add(new Carta(Valor.OITO, Nipe.COPAS))
