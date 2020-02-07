61d415c1856338368531310a987a541572dbc550
emanuel.passinato <emanuel.passinato@zgsolucoes.com.br>
2020-02-07 14:54:56 -0300

    Terminando a Criação de objetos

--- src/main/groovy/ApiGetter.groovy
+++ src/main/groovy/ApiGetter.groovy
@@ -2,6 +2,7 @@ class ApiGetter {
 
 	static String obtemJson(){
 		URLConnection get = new URL("http://172.22.0.255:3333/dados-pokerhand").openConnection()
+		get.setReadTimeout(300000000i)
 		String jsonResponse = get.getInputStream().getText()
 		return jsonResponse
 	}
---
+++ src/main/groovy/CriadorObjetos.groovy
@@ -0,0 +1,22 @@
+import dominio.Rodada
+import groovy.json.JsonSlurper
+import groovy.json.internal.LazyMap
+import services.RodadaService
+
+class CriadorObjetos {
+
+	List<Rodada> cria(String json) {
+
+		RodadaService rodadaService = new RodadaService()
+
+		JsonSlurper jsonSlurper = new JsonSlurper()
+		List<LazyMap> jogos = jsonSlurper.parseText(json) as List<LazyMap>
+
+		List<Rodada> todasRodadas = []
+		for (LazyMap jogo in jogos) {
+			LazyMap rodadas = jogo.values().flatten() as LazyMap
+			todasRodadas.addAll(rodadaService.criaRodadas(rodadas))
+		}
+		return todasRodadas
+	}
+}
